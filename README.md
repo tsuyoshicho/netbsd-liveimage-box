1a1ca3dc57c6511b0ca38f6116f9a7a007ab5679
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2015-09-22 12:49:59 +0900

    draft files

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # netbsd-liveimage-box
 Create NetBSD Live Image vagrant box (from http://www.ceres.dti.ne.jp/tsutsui/netbsd/liveimage/ )
+
+# Create Vagrant box for NetBSD Live Image
+
+## source
+http://www.ceres.dti.ne.jp/tsutsui/netbsd/liveimage/
+
+## recipe
+* packer
+* vdi -> vmdk
+* uuid unique
+* push to atlas
+
---
+++ netbsd_setup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# need tools
+# sed,curl,git
+# packer,virtualbox,vagrant
+
+# filename setup
+VDI_FILE=test.vdi
+VMDK_FILE=test.vmdk
+TEMPLATE_FILE=test.ovf
+RESULT_FILE=result.ovf
+OVA_FILE=test.ova
+ARCHIVE_FILE=test.zip
+
+UUID_RESULT=uuid_result.txt
+UUID_FILE=uuid.txt
+
+# 1.download vdi
+curl -L -O $URL -o $ARCHIVE_FILE
+
+# 2.git clone for ovf template file
+#git clone <url>
+
+# 3.file name setup
+
+# 4.uuid fixup
+# replace for vid UUID(auto generate)
+$VBOX_MSI_INSTALL_PATH/vboxmanage internalcommands sethduuid
+$VDI_FILE > $UUID_RESULT
+
+# split only UUID
+cat uuid_result.txt | sed -e s/[^:]*: *\([0-9a-fA-F-]*\).*/\1/ >
+$UUID_FILE
+
+UUID=$(cat $UUID_FILE)
+
+# replace for file UUID
+sed -e s/UUIDREPLACE/$UUID/g $TEMPLATE_FILE > $RESULT_FILE
+
+# rm files
+rm $UUID_FILE $UUID_RESULT
+
+
+# 5.buildup box(packer)
+# convert vdi to vmdk
+$VBOX_MSI_INSTALL_PATH/vboxmanage clonehd --format VMDK
+$VDI_FILE $VMDK_FILE
+
+# 6.push vagrant box to atlas?
+
